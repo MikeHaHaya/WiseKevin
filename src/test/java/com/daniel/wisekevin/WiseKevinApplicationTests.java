847ab55f2a2ae7b366208e2d5173656d2ee6052a
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2022-08-11 17:22:17 +0300

    Initial commit

---
+++ src/test/java/com/daniel/wisekevin/WiseKevinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.wisekevin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiseKevinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
