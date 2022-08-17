b0cca92aa91f8051a5e10d2df287c9ee27b737a7
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2022-08-17 20:42:31 +0300

    TODOs

--- src/main/java/app/core/WiseKevinApplication.java
+++ src/main/java/app/core/WiseKevinApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WiseKevinApplication {
 
+    // TODO -- Insert more SQL and database technology into the project
     public static void main(String[] args) {
         SpringApplication.run(WiseKevinApplication.class, args);
 
