be03f0d87e2317b490e6ee9a1c406a2dfee21269
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2022-11-18 09:39:42 +0200

    Removed Springboot uses

--- src/main/java/app/core/WiseKevinApplication.java
+++ src/main/java/app/core/WiseKevinApplication.java
@@ -1,12 +1,8 @@
 package app.core;
 
-import static app.core.statics.Globals.*;
-
 import app.core.game.GameFrame;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
+
 public class WiseKevinApplication {
 
     public static void main(String[] args) {
